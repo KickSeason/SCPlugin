b0470c1aef44b4d4d190e8a154cb8db1c6b222a1
KickSeason <zhang2010tao@sina.com>
2018-12-05 18:07:55 +0800

    add help

--- SCPlugin/SCPlugin.cs
+++ SCPlugin/SCPlugin.cs
@@ -6,32 +6,67 @@ using Neo;
 using Neo.VM;
 using Neo.Plugins;
 using Neo.SmartContract;
+using Neo.IO.Json;
 
 namespace Neo.Plugins
 {
     public class SCPlugin : Plugin, ICmdPlugin
     {
-        public override string Name { get { return "testsc"; } }
+        public SCPlugin()
+        {
+            StandardService.Notify += this.notify;
+            StandardService.Log += this.log;
+        }
+        public override string Name
+        {
+            get
+            {
+                return "testsc";
+            }
+        }
         public bool Exec(string[]args)
         {
-            if (CheckArgs(args, 1))
+            if (checkArgs(args, 1))
             {
-                Console.WriteLine(args[0]);
-                var script = File.ReadAllBytes(args[0]);
-                Console.WriteLine("script: {0}", script.ToHexString());
-                return InvokeScript(script);
+                try
+                {
+                    var script = File.ReadAllBytes(args[0]);
+                    Console.WriteLine("sc path: {0}", args[0]);
+                    Console.WriteLine("script: {0}", script.ToHexString());
+                    return InvokeScript(script);
+                } catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return true;
+                }
             }
             Console.WriteLine("wrong args count.");
-            return false;
+            return true;
         }
-        private bool CheckArgs(string[] args, int n)
+        public string HelpStr
+        {
+            get
+            {
+                var str = "\t" + this.Name + " [.avm] test your smart contract.\n";
+                return str;
+            }
+        }
+        private bool checkArgs(string[] args, int n)
         {
             if (args.Length < n) return false;
             return true;
         }
+        public void notify(Object o, NotifyEventArgs args)
+        {
+            Console.WriteLine("[Notify] ScriptHash: {0} State: {1}", args.ScriptHash, args.State.ToParameter().ToJson().ToString());
+        }
+        public void log(Object o, LogEventArgs args)
+        {
+            Console.WriteLine("[Log] ScriptHash: {0} Message: {1}", args.ScriptHash, args.Message);
+        }
         private bool InvokeScript(byte[] script)
         {
-            ApplicationEngine engine = ApplicationEngine.Run(script, null, null, true, new Fixed8(1000));
+            ApplicationEngine engine = ApplicationEngine.Run(script, null, null, true, default(Fixed8));
             Console.WriteLine("state:{0}", engine.State);
             Console.WriteLine("gas_consumed:{0}", engine.GasConsumed.ToString());
             Console.Write("stack: [\n");
